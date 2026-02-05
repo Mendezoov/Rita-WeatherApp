4e957e81a131ca5c95c99e8c43fb645d14a7bd77
Mendezoov <191722646+Mendezoov@users.noreply.github.com>
2026-02-05 03:43:27 +0200

    Initial Commit

---
+++ NetworkingModule/NetworkingModuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingModuleApp.swift
+//  NetworkingModule
+//
+//  Created by Mendez on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingModuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkingModuleTests/NetworkingModuleTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingModuleTests.swift
+//  NetworkingModuleTests
+//
+//  Created by Mendez on 2/5/26.
+//
+
+import Testing
+@testable import NetworkingModule
+
+struct NetworkingModuleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
